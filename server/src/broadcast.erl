4051159a0c78e4fdcd930ceaaafd0303ee55c2af
lukas <lukas.block@pm.me>
2023-04-28 22:13:45 +0200

    add simple interaction

--- server/src/broadcast.erl
+++ server/src/broadcast.erl
@@ -9,18 +9,12 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, format_status/2]).
 
+-export([add_player/1, remove_player/1, send_action/2, list_player/0]).
+
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {player=[]}).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
-
--spec start_link() -> {ok, Pid :: pid()} |
-          {error, Error :: {already_started, pid()}} |
-          {error, Error :: term()} |
-          ignore.
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
@@ -28,6 +22,18 @@ init([]) ->
     process_flag(trap_exit, true),
     {ok, #state{}}.
 
+handle_call({add, Player}, _From, State) ->
+    NewPlayerList = lists:append(State#state.player, [Player]),
+    NewState = State#state{player=NewPlayerList},
+    {reply, {ok, NewState}, NewState};
+
+handle_call({remove, Player}, _From, State) ->
+    NewPlayerList = lists:delete(Player, State#state.player),
+    NewState = State#state{player=NewPlayerList},
+    {reply, {ok, NewState}, NewState};
+
+handle_call(list, _From, State) ->
+    {reply, State#state.player, State};
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -48,3 +54,9 @@ code_change(_OldVsn, State, _Extra) ->
 
 format_status(_Opt, Status) ->
     Status.
+
+
+add_player(Player) -> gen_server:call(?SERVER, {add, Player}).
+remove_player(Player) -> gen_server:call(?SERVER, {remove, Player}).
+send_action(Player, Action) -> gen_server:call(?SERVER, {send_action, Player, Action}).
+list_player() -> gen_server:call(?SERVER, list).
