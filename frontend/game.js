478c8a4f4b3beb153237c3e96bdcc6fdfdc158ff
lhrb <lukas.block@pm.me>
2023-04-29 22:33:50 +0200

    poc sync movement via ws

--- frontend/game.js
+++ frontend/game.js
@@ -1,5 +1,9 @@
 import Phaser from 'phaser'
 
+let leftKey;
+let rightKey;
+let player;
+
 const ws = new WebSocket("ws://localhost:8080/websocket/");
 
 // Connection opened
@@ -9,8 +13,16 @@ ws.addEventListener("open", (event) => {
 
 // Listen for messages
 ws.addEventListener("message", (event) => {
-  console.log("Message from server ", event.data);
+    console.log("Message from server ", event.data);
+    const action = JSON.parse(event.data);
+    switch (action.action) {
+        case "move":
+            player.x = action.x;
+            break;
+    }
 });
+
+
 
 class Example extends Phaser.Scene
 {
@@ -25,21 +37,35 @@ class Example extends Phaser.Scene
     }
 
     create () {
-        this.m = this.add.sprite(200, 200, 'monk', 'idle/idle_1.png');
-        this.m.setScale(4,4);
+
+        leftKey = this.input.keyboard.addKey('LEFT');
+        rightKey = this.input.keyboard.addKey('RIGHT');
+
+
+        player = this.add.sprite(200, 200, 'monk', 'idle/idle_1.png');
+        player.setScale(4,4);
         var idleFrames = this.anims.generateFrameNames('monk', {
             start: 1, end: 6, zeroPad:0,
             prefix: 'idle/idle_', suffix: '.png'
         });
         this.anims.create({ key: 'idle', frames: idleFrames, frameRate: 10, repeat: -1 });
-        this.m.anims.play('idle');
+        player.anims.play('idle');
     }
 
     update(time, delta) {
-        this.m.x += delta/8;
+
+        if (leftKey.isDown) {
+            player.x -= delta/8;
+        } else if (rightKey.isDown) {
+            player.x += delta/8;
+        }
+
+        /*this.m.x += delta/8;
         if (this.m.x > 800) {
             this.m.x = -50;
-        }
+        }*/
+
+        ws.send(JSON.stringify({action: "move", x: player.x}));
     }
 }
 
