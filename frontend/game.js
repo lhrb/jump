6c1c232887bd3bf064bc28593b3879561c3d296f
lhrb <lukas.block@pm.me>
2023-05-07 21:59:49 +0200

    experiment polygon controls

--- frontend/game.js
+++ frontend/game.js
@@ -37,9 +37,38 @@ ws.addEventListener("message", (event) => {
     }
 });
 
-function playAnim(char, anim) {
-    char.anims.play(monk.anim(anim), true)
-        .once('animationcomplete', () => char.anims.play(monk.anim('idle')));
+function polygonPoints(x0, y0, r1, r2, offset) {
+    const off = 72 * offset;
+    // 72 degree split into 8
+    const deg = [...Array(9).keys()]
+        .map(i => i * 9 + off)
+        .map(d => Phaser.Math.DegToRad(d));
+
+    const inner = deg.map(d => {
+            const c = Math.cos(d);
+            const s = Math.sin(d);
+            // inner
+            const x = x0 + r1 * c;
+            const y = y0 + r1 * s;
+            return new Phaser.Geom.Point(x, y)
+          });
+
+    const outer = deg.map(d => {
+        const c = Math.cos(d);
+        const s = Math.sin(d);
+        // outer
+        const x = x0 + r2 * c;
+        const y = y0 + r2 * s;
+        return new Phaser.Geom.Point(x, y);
+    }).reverse()
+
+    return inner.concat(outer);
+}
+
+function polygonCenter(points) {
+    const x = points.map(p => p.x).reduce((acc, e) => acc + e, 0) / points.length;
+    const y = points.map(p => p.y).reduce((acc, e) => acc + e, 0) / points.length;
+    return new Phaser.Geom.Point(x, y);
 }
 
 class Example extends Phaser.Scene
@@ -58,6 +87,26 @@ class Example extends Phaser.Scene
     }
 
     create () {
+        // Create the middle button
+        const middleButton = this.add.circle(400, 300, 25, 0xff0000);
+        middleButton.setInteractive();
+        middleButton.on('pointerdown', () => {
+            console.log('Middle button clicked');
+        });
+
+        for (let i = 0; i < 5; i++) {
+            const points = polygonPoints(400, 300, 25, 40, i);
+            const polygonButton = this.add.polygon(0, 0, points, 0x2ecc71);
+            polygonButton.setInteractive(new Phaser.Geom.Polygon(points), Phaser.Geom.Polygon.Contains);
+            //polygonButton.setPosition(400,300);
+
+            polygonButton.on('pointerdown', () => {
+                console.log('Polygon button clicked!' + i);
+            });
+            console.log(polygonButton);
+        }
+
+
 
         leftKey = this.input.keyboard.addKey('LEFT');
         rightKey = this.input.keyboard.addKey('RIGHT');
@@ -65,13 +114,13 @@ class Example extends Phaser.Scene
 
         healthBar = this.add.graphics();
         healthBar.fillStyle(0xe74c3c, 1);
-        healthBar.fillRect(10,10,200,20);
+        healthBar.fillRect(10,10,200,10);
 
         staminaBar = this.add.graphics();
         staminaBar.fillStyle(0x2ecc71, 1);
-        staminaBar.fillRect(10,35,200,20);
+        staminaBar.fillRect(10,25,200,10);
 
-        buttonA.gameObject = this.add.circle(780, 330, 15, 0x2ecc71).setInteractive();
+        buttonA.gameObject = this.add.circle(780, 330, 25, 0x2ecc71).setInteractive();
         buttonB.gameObject = this.add.circle(755, 305, 15, 0x2ecc71).setInteractive();
         buttonC.gameObject = this.add.circle(780, 280, 15, 0x2ecc71).setInteractive();
         buttonD.gameObject = this.add.circle(805, 305, 15, 0x2ecc71).setInteractive();
